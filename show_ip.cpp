696856efbc904ee476e730e5b95a63b30e3a6cf7
Lev <lev8@pdx.edu>
2024-07-18 12:35:36 -0700

    network_data.cpp is a test file which sets up socket address data structures show_ip outputs the ip address of whatever you type into the command line when you run the program Not sure why my C++ version of show_ip doesn't work - possible minor language differences I don't know

---
+++ show_ip.cpp
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    struct addrinfo objA;
+    struct addrinfo *objAresults;
+    struct addrinfo *objAptr;
+   
+    int status;
+    
+    char ip[INET6_ADDRSTRLEN];
+
+    if (argc != 2) 
+    {
+        cerr << "Error." << endl;   
+        return 1;
+    }
+
+
+    memset(&objA, 0, sizeof(objA));
+    objA.ai_family = AF_UNSPEC;
+    objA.ai_socktype = SOCK_STREAM;
+
+    if ((status = getaddrinfo(argv[1], NULL, &objA, &objAresults)) != 0)
+    {
+        cerr << "getaddrinfo() error: " << gai_strerror(status);
+        return -1;
+    }
+
+    cout << "IP Addresses for: " << argv[1] << endl;
+
+    for (objAptr = objAresults; objAptr != nullptr; objAptr = objAptr->ai_next)
+    {
+        void *addr;
+        char *ipver;
+        
+        if (objAptr->ai_family == AF_INET) 
+        {   
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)objAptr->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        }
+        else 
+        {
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)objAptr->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+    }
+        
+    inet_ntop(objAptr->ai_family, addr, ipstr, sizeof(ipstr));
+    cout << ipver << "\n" << ipstr << endl;
+
+    freeaddrinfo(objAresults);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
