c6f764db8327cfb6d1e8585548a2d05490aaedcc
Lev <lev8@pdx.edu>
2024-07-22 13:04:51 -0700

    We now have a working loopback socket connection between server.py and client.py !

--- client.py
+++ client.py
@@ -1,11 +1,30 @@
 import socket
 
-sObj = socket.socket()
+"""
+Ask the OS for a socket.
+How do I do UNSPEC??? INET enables IPv4 only.
+SOCK_STREAM enables TCP only.
+"""
+sObj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-port = 12345
+"""Create a port integer."""
+port = 16327
 
-sObj.connect(('127.0.0.1', port))
+"""
+Connect to an IP address on a specific port.
+Connection will fail if there is no server listening on said port.
+"""
+while True:
+    try:
+        sObj.connect(('127.0.0.1', port))
+    except:
+        print("Problem connecting.\n")
+    else:
+        print("Connection successful.\n")
+    
+    data = sObj.recv(4096) 
+    print(data.decode())
 
-print(sObj.recv(1024).decode())
-
-sObj.close()
+    sObj.close()
+    
+    break
--- server.py
+++ server.py
@@ -1,21 +1,70 @@
 import socket
 
-sObj = socket.socket()
+"""
+Ask the OS for a socket.
+How do I do UNSPEC??? INET enables IPv4 only.
+SOCK_STREAM enables TCP only.
+"""
+sObj = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#socket.socket(socket.AF_PASSIVE, socket.SOCK_STREAM)
+"""Create a port integer."""
+port = 16327
 
-port = 12345
+"""
+Bind the socket to our port.
+127.0.0.1 is the loopback host (means we connect to our computer).
+Note that nothing is connected yet - just setting things up.
+"""
+try:
+    sObj.bind(('127.0.0.1', port))
+except:
+    print(f"Error binding socket to port.")
+    #print(sObj.gaierror()) #This line might be wrong.
+    exit()
+else:
+    print(f"Socket bound to port: {port}.")
 
-sObj.bind(('127.0.0.1', port))
-
-print(f"Socket bound to {port}.")
-
+"""
+Listen for incoming connections.
+When found, accept the client.
+Arg is 0 because there shouldn't be any unaccepted connections
+before we connect to backlog (our own computer).
+"""
 while True:
-    c, addr = sObj.accept()
-    print(f"Got connection from {addr}.")
+    try: 
+        sObj.listen(0)
+    except:
+        print("La la la la la, not listening!\n")
+        #print(sObj.gaierror()) #I think there's a different error check method for this.
+        exit()
+    else:
+        print("Listening...\n")
     
-    c.send("Thank you for connecting.".encode())
+    #Connection is a new socket obj, address is that bound to socket on client side.
+    try:
+        connectedSocket, clientAddress = sObj.accept()
+    except: 
+        print("Did not accept.\n")
+        #print(sObj.gaierror()) #Maybe?
+        exit()
+    else:
+        print(f"Got connection from {clientAddress}.")
     
-    c.close()
+    #Send data over the socket. 
+    msg = "Thank you for the connection!"
+    try:
+        connectedSocket.send(msg.encode())
+    except:
+        print("Error sending.")
+        #print(connectedSocket.gaierror()) #Maybe?
+        exit()
+    else:
+        print("Message sent!")
+    
+    connectedSocket.close()
     
     break
+
+
+
+
