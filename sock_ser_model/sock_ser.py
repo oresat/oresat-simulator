5a840f5a9a8574a839997723e58bee53440e734e
Lev <lev8@pdx.edu>
2024-08-06 16:42:06 -0700

    Server now remains when client closes, can reconnect client at will

--- sock_ser_model/sock_ser.py
+++ sock_ser_model/sock_ser.py
@@ -24,17 +24,25 @@ Bind the socket to our port.
 127.0.0.1 is the loopback host (means we connect to our computer).
 Note that nothing is connected yet - just setting things up.
 """
+
 sock.bind(('127.0.0.1', port))
 sock.listen()
-cSock, cAddr = sock.accept()
 
+while True:
+    cSock, cAddr = sock.accept()
 
-with serial.Serial(port = "/dev/ttyUSB0", baudrate = 115200) as ser:
-    try:
-        while True:
-            data = cSock.recv(4096) + b"\n\r"
-            scribe = ser.write(data) #Ask the scribe to return the msg
+    with serial.Serial(port = "/dev/ttyUSB0", baudrate = 115200) as ser:
+        try:
+            while True:
+                data = cSock.recv(4096) + b"\n\r"
+                scribe = ser.write(data) #Ask the scribe to return the msg
+                """
+                Check if socket is still connected.
+                """
+                check_socket = cSock.recv(socket.MSG_DONTWAIT | socket.MSG_PEEK)
+                if len(check_socket) == 0:
+                    break
 
-    except KeyboardInterrupt:
-        print("\nEnded via ctrl-c.\n") 
+        except KeyboardInterrupt:
+            print("\nEnded via ctrl-c.\n") 
 
