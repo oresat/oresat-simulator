dc11ceb081ec7a209a83d54fc9345f03024f48d9
Lev <lev8@pdx.edu>
2024-09-01 16:58:10 -0700

    Modularizing pack/unpack feature
    I want to be able to call functions rather than have to rewrite
    things, but it's difficult to figure out which arguments should
    be there for the unpack function.

--- s2sServer.py
+++ s2sServer.py
@@ -55,3 +55,4 @@ if __name__ == '__main__':
                         print("\nEnded via ctrl-c.\n") 
                 else:
                     try:
+                        
--- sock_ser_model/tempClient.py
+++ sock_ser_model/tempClient.py
@@ -12,21 +12,23 @@ array = []
 
 loopback = "127.0.0.1"
 
-for x in range(10):
-    array.append(Element(10.6, 10.7, 10.8))
+def pack_array_data(array):
+    for x in range(10):
+        array.append(Element(10.6, 10.7, 10.8))
 
-#Note that this array is 10 elements x 3 floats x 4 bytes = 120 bytes
+    to_send = bytes()
+    #for i in range(len(array)):
+    for item in array:
+        to_send += struct.pack('3f', item.x, item.y, item.z)
+    return to_send 
+
+pack_array_data(array)
 print(array)
-
-to_send = bytes()
-#for i in range(len(array)):
-for item in array:
-    to_send += struct.pack('3f', item.x, item.y, item.z)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     
     port = 40000
     
     sock.connect(("127.0.0.1", port))
-    
+   
     sock.send(to_send)
