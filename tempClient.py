d4c845a9e595fd72d5b8b77b738a2d6551859ecc
Lev <lev8@pdx.edu>
2024-09-04 13:23:14 -0700

    Combining data parsing and socket/serial sending
    If my data is unpacked it doesn't send over serial, but it was
    originally packed via struct, so I don't know why once it's sent over socket
    it isn't as easy as continuing to send it along over serial, since it
    should already be in byte format

--- tempClient.py
+++ tempClient.py
@@ -2,28 +2,28 @@
 import struct
 import socket
 from dataclasses import dataclass
+import pprint
 
-@dataclass 
-class Element:
-    x: float
-    y: float
-    z: float
 array = []
 
 loopback = "127.0.0.1"
+x = 1.5
+y = 2.5
+z = 3.5
 
 def pack_array_data(array):
-    for x in range(10):
-        array.append(Element(10.6, 10.7, 10.8))
+    for a in range(10):
+        array.append([x, y, z])
 
     to_send = bytes()
     #for i in range(len(array)):
     for item in array:
-        to_send += struct.pack('3f', item.x, item.y, item.z)
+        for i in range(3):
+            to_send += struct.pack('f', item[i])
     return to_send 
 
-pack_array_data(array)
-print(array)
+cargo = pack_array_data(array)
+pprint.pp(array)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     
@@ -31,4 +31,4 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     
     sock.connect(("127.0.0.1", port))
    
-    sock.send(to_send)
+    sock.send(cargo) 
