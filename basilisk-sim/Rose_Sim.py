28ae74bce37007a729dfe1531e3cfcc45ead07b0
Max Eltzroth <mee5@pdx.edu>
2025-01-29 15:21:38 -0800

    Updated multithread to work on solar simulator

--- basilisk-sim/Rose_Sim.py
+++ basilisk-sim/Rose_Sim.py
@@ -206,7 +206,7 @@ def run(show_plots, livestream, step_time, stop_time, rI, init_pos, init_vel, in
         for line in mag_data:
             fd.write(",".join([str(thing) for thing in line]) + "\n")
 
-    all_data = {"Header":["time","sun_exposure", "sun_x+", "sun_x-", "sun_y+", "sun_y-", "sun_z+", "sun_z-","mag_x", "mag_y", "mag_z"]}
+    all_data = {"header":["time","sun_exposure", "sun_x+", "sun_x-", "sun_y+", "sun_y-", "sun_z+", "sun_z-","mag_x", "mag_y", "mag_z"]}
     for ii in range(len(epoch_timestamp)):
         all_data.update({ epoch_timestamp[ii]: [float(eclipseData[ii])] + [direction[ii] for direction in sunData.values()] + [float(magd) for magd in magData[ii]]})
 
--- basilisk-sim/multithread_test.py
+++ basilisk-sim/multithread_test.py
@@ -81,34 +81,57 @@ def all_data(conn, shared_data):
 
 def solar_data(conn, shared_data):
     output_numbers = conn.recv()
-        
-    ser = serial.Serial(
-      port= '/dev/ttyACM0',
-       )
-
-    while True:
-        wait_time = 1 - (time.time() % 1)
-        time.sleep(wait_time)
-
-        epoch = time.time()
-        my_list = shared_data.get(int(epoch))
-        
-        if my_list is None:
-            print("ran out of data at ", epoch)
-            break
-        
-        print(epoch, my_list)
-
-        #print(epoch)
-        #print(int(epoch))
-        #print(the_line[3])
-        #print(the_line[4])
-        ser.write(my_list[3])
-        #time.sleep(1) #seconds to pause (ONLY OUTPUT)
+    
+    has_serial = False
+    try:
+        ser = serial.Serial(
+                port= '/dev/ttyACM0',
+                baudrate = 115200
+                )
+        has_serial = True
+    except:
+        print("Failed to connect to serial, will only print numbers to terminal.")
 
 
+    header = shared_data["header"]
+    print(shared_data["header"])
 
-    ser.close()
+    if has_serial:
+        ser.write(3)
+
+    time.sleep(1)
+
+
+    try:
+
+        while True:
+            wait_time = 1 - (time.time() % 1)
+            time.sleep(wait_time)
+
+            epoch = time.time()
+            my_list = shared_data.get(int(epoch))
+            
+            if my_list is None:
+                print("ran out of data at ", epoch)
+                break
+            
+            print(epoch, my_list)
+
+            print("column index", column_index := header.index("sun_x+"))
+            
+            value_to_send = int(100*my_list[column_index])
+
+            if has_serial:
+                ser.write((str(value_to_send) + "\n").encode("utf-8"))
+
+    except e:
+        print("An error occured", e)
+    finally:
+        if has_serial:
+            ser.write("0\n".encode("utf-8"))
+            print("Attempted to turn off solar simulator")
+            ser.close()
+            print("Closed serial")
 
 
 
